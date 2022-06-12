b7d64f3233c7aa5afbf86b80c193896b4ced698a
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-06-12 16:55:12 -0500

    created user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
+
+//export the User model
+module.exports = User;
